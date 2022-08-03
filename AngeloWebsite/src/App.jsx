e30c3926ee972300491425ab916bf6d9b379425a
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2022-08-03 03:05:49 -0400

    (+)react

---
+++ AngeloWebsite/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+/*
+import reactLogo from './assets/react.svg'
+<img width="60px" src="/vite.svg" className="logo" alt="Vite logo" />
+<img src={reactLogo} className="logo react" alt="React logo" />
+*/
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
