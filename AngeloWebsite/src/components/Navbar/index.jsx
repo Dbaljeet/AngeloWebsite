e924c31718dc4d2b78783c2e2e24ed7aecb94ebf
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2022-09-10 00:08:04 -0300

    [Fix] accesibility, fonts and navbar

--- AngeloWebsite/src/components/Navbar/index.jsx
+++ AngeloWebsite/src/components/Navbar/index.jsx
@@ -1,53 +1,130 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
-import { Nav , Lista, Links, Hamburger} from "./stylesNavbar";
-const Navbar = () =>{
-    const [show, setShow] = useState(false)
-    const [isMobile, setisMobile] = useState(window.innerWidth<622)
-    const ShowMenu = () =>{
-        !show ?
-        setShow(true)
-        :
-        setShow(false)
-    }
-    
-    const updateSize = (ev) =>{
-        setisMobile(ev.target.innerWidth<622)
-    }
+import { Nav, Lista, Links, Hamburger } from "./stylesNavbar";
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const [isMobile, setisMobile] = useState(window.innerWidth < 622);
+  const ShowMenu = () => {
+    !show ? setShow(true) : setShow(false);
+  };
 
-    useEffect (()=>{
-        window.addEventListener('resize', updateSize);
-        return () => window.removeEventListener('resize', updateSize)
-    },[])
+  const updateSize = (ev) => {
+    setisMobile(ev.target.innerWidth < 622);
+  };
 
-    return(
-        <>
-            <Nav className='burguer' open={show}>
-                <Hamburger id={`${show ? 'open' : ''}`} onClick={ShowMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Hamburger>
+  useEffect(() => {
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
-                { isMobile &&
-                <Lista className={`${show? 'active':'desactive'}`}>
-                    <Links onClick={ShowMenu} to="Home" spy={true} smooth={true} offset={-300} duration={500} isDynamic={true}>Inicio</Links> 
-                    <Links onClick={ShowMenu} to="Portfolio" spy={true} smooth={true} offset={-350} duration={500} isDynamic={true}>Portfolio</Links>    
-                    <Links onClick={ShowMenu} to="Experience" spy={true} smooth={true} offset={-350} duration={500} isDynamic={true}>Conocimientos</Links>  
-                    <Links onClick={ShowMenu} to="Contact" spy={true} smooth={true} offset={-350} duration={500} isDynamic={true}>Contacto</Links>  
-                </Lista>
-                }
+  return (
+    <>
+      <Nav className="burguer" open={show}>
+        <Hamburger id={`${show ? "open" : ""}`} onClick={ShowMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </Hamburger>
 
-                { !isMobile &&
-                <Lista className={`${show? 'active':'desactive'}`}>
-                    <Links onClick={ShowMenu} to="Home" spy={true} smooth={true} offset={-100} duration={500} isDynamic={true}>Inicio</Links> 
-                    <Links onClick={ShowMenu} to="Portfolio" spy={true} smooth={true} offset={-25} duration={500} isDynamic={true}>Portfolio</Links>    
-                    <Links onClick={ShowMenu} to="Experience" spy={true} smooth={true} offset={-30} duration={500} isDynamic={true}>Conocimientos</Links>   
-                    <Links onClick={ShowMenu} to="Contact" spy={true} smooth={true} offset={-60} duration={500} isDynamic={true}>Contacto</Links>  
-                </Lista>
-                }
-            </Nav>
-        </>
-    )
-}
-export default Navbar
+        {isMobile && (
+          <Lista className={`${show ? "active" : "desactive"}`}>
+            <Links
+              onClick={ShowMenu}
+              to="Home"
+              spy={true}
+              smooth={true}
+              offset={-300}
+              duration={500}
+              isDynamic={true}
+            >
+              Inicio
+            </Links>
+            <Links
+              onClick={ShowMenu}
+              to="Portfolio"
+              spy={true}
+              smooth={true}
+              offset={-350}
+              duration={500}
+              isDynamic={true}
+            >
+              Portfolio
+            </Links>
+            <Links
+              onClick={ShowMenu}
+              to="Experience"
+              spy={true}
+              smooth={true}
+              offset={-350}
+              duration={500}
+              isDynamic={true}
+            >
+              Conocimientos
+            </Links>
+            <Links
+              onClick={ShowMenu}
+              to="Contact"
+              spy={true}
+              smooth={true}
+              offset={-350}
+              duration={500}
+              isDynamic={true}
+            >
+              Contacto
+            </Links>
+          </Lista>
+        )}
+
+        {!isMobile && (
+          <Lista className={`${show ? "active" : "desactive"}`}>
+            <Links
+              onClick={ShowMenu}
+              to="Home"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              isDynamic={true}
+            >
+              Inicio
+            </Links>
+            <Links
+              onClick={ShowMenu}
+              to="Portfolio"
+              spy={true}
+              smooth={true}
+              offset={-25}
+              duration={500}
+              isDynamic={true}
+            >
+              Portfolio
+            </Links>
+            <Links
+              onClick={ShowMenu}
+              to="Experience"
+              spy={true}
+              smooth={true}
+              offset={-30}
+              duration={500}
+              isDynamic={true}
+            >
+              Conocimientos
+            </Links>
+            <Links
+              onClick={ShowMenu}
+              to="Contact"
+              spy={true}
+              smooth={true}
+              offset={-60}
+              duration={500}
+              isDynamic={true}
+            >
+              Contacto
+            </Links>
+          </Lista>
+        )}
+      </Nav>
+    </>
+  );
+};
+export default Navbar;
--- AngeloWebsite/src/pages/Home.jsx
+++ AngeloWebsite/src/pages/Home.jsx
@@ -1,22 +1,20 @@
 import React from "react";
-import { PrincipalTitle } from '../components/Titles'
+import { PrincipalTitle } from "../components/Titles";
 import Portfolio from "../components/Portfolio/Portfolio";
 import About from "../components/About";
 import Contact from "../components/Contact";
-import Experience from '../components/Experience'
+import Experience from "../components/Experience";
 import MoreInfo from "../components/MoreInfo";
-const Home = () =>{
-    return (
-        <>
-            
-            <PrincipalTitle>Angelo Berrios Pinto</PrincipalTitle>
-            <About></About>
-            <Portfolio></Portfolio>
-            <Experience></Experience>
-            <Contact></Contact>
-            <MoreInfo></MoreInfo>
-            
-        </>
-    )
-}
-export default Home
+const Home = () => {
+  return (
+    <>
+      <PrincipalTitle id="Home">Angelo Berrios Pinto</PrincipalTitle>
+      <About></About>
+      <Portfolio></Portfolio>
+      <Experience></Experience>
+      <Contact></Contact>
+      <MoreInfo></MoreInfo>
+    </>
+  );
+};
+export default Home;
